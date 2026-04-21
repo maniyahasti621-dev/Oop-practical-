39d296ddda5c2187459ead485863b9c785cb9467
maniyahasti621-dev <maniyahasti621@gmail.com>
2026-04-21 10:46:43 +0530

    Practical 1

---
+++ Practical 1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Practical1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+        
+        double feet = meters * 3.28084;
+        
+        // Display result formatted to two decimal places
+        System.out.printf("%.2f meters = %.2f feet\n", meters, feet);
+        
+        sc.close();
+    }
+}
