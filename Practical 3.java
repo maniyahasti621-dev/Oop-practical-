c0c3670be030245657e31cdde9b8c92e68f0f49e
maniyahasti621-dev <maniyahasti621@gmail.com>
2026-04-22 17:08:22 +0530

    Practical 3.java

--- Practical 3.java
+++ Practical 3.java
@@ -7,10 +7,8 @@ public class Practical3{
         System.out.print("Enter a single letter: ");
         char ch = sc.next().charAt(0);
 
-        // Convert to lowercase for case-insensitive check
         char lowerCh = Character.toLowerCase(ch);
 
-        // Check if it's a letter first
         if (Character.isLetter(lowerCh)) {
             if (lowerCh == 'a' || lowerCh == 'e' || lowerCh == 'i' || lowerCh == 'o' || lowerCh == 'u') {
                 System.out.println(ch + " is a vowel");
